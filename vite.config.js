96987c4fd186b873586e0a93db21a6f477507e0b
Rivaldo Guterres <rj142872@gmail.com>
2025-05-09 02:35:12 +0900

    Change to url server

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,9 @@ import { fileURLToPath, URL } from 'url'
 
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    host: '0.0.0.0', // <- agar bisa diakses dari komputer lain
-    port: 5173
-  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)) 
     }
   }
 })
-
