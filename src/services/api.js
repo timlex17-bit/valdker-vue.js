e4412b1576ee741df4d78c73501eb3d012136aad
Rivaldo Guterres <rj142872@gmail.com>
2025-05-09 02:36:28 +0900

    Update chnage to api server

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const API_BASE = 'https://mypos-production.up.railway.app//api'; // ganti sesuai IP server Anda
+const API_BASE = 'https://mypos-production.up.railway.app/api'; // ganti sesuai IP server Anda
 
 export const fetchProducts = () => axios.get(`${API_BASE}/products/`);
 export const fetchCategories = () => axios.get(`${API_BASE}/categories/`);
-export const fetchBanners = () => axios.get('https://mypos-production.up.railway.app//api/banners/');
+export const fetchBanners = () => axios.get('https://mypos-production.up.railway.app/api/banners/');
 
 export const createOrder = (orderData) => axios.post(`${API_BASE}/orders/`, orderData);
 
